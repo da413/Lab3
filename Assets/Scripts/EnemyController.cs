e4a61d1e04c3306e32b15e8928bb245d2bd16980
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2025-09-10 17:30:19 -0400

    script mostly finished

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
-    public float rotationSpeed = 10.0f;
     public GameObject player;
+    public float baseOrbitSpeed = 0.5f;
+    public float orbitSpeedMultiplier = 0.5f;
+    private float orbitAngle;
+    public float minOrbitDistance = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 offset = transform.position - player.transform.position;
+        orbitAngle = Mathf.Atan2(offset.z, offset.x);
     }
 
     // Update is called once per frame
     void Update()
     {
-        var directionToPlayer = Vector3.Normalize(player.transform.position - transform.position);
-        if (Vector3.Dot(transform.TransformDirection(Vector3.up), directionToPlayer) > 0 )
-        {
-            //transform.rotation = Quaternion.Slerp();
-        }
+        Vector3 directionToPlayer = player.transform.position - transform.position;
+
+        float distanceToPlayer = directionToPlayer.magnitude;
+        distanceToPlayer = Mathf.Max(distanceToPlayer, minOrbitDistance);
+
+        float orbitSpeed = baseOrbitSpeed + distanceToPlayer * orbitSpeedMultiplier;
+        orbitAngle += orbitSpeed * Time.deltaTime;
+
+        float x = player.transform.position.x + Mathf.Cos(orbitAngle) * distanceToPlayer;
+        float z = player.transform.position.z + Mathf.Sin(orbitAngle) * distanceToPlayer;
+        transform.position = new Vector3(x, transform.position.y, z);
+
+        float targetAngle = Mathf.Atan2(directionToPlayer.x, directionToPlayer.z) * Mathf.Rad2Deg;
+        Vector3 currentRotation = transform.eulerAngles;
+        currentRotation.y += Mathf.DeltaAngle(currentRotation.y, targetAngle - 90f);
+        transform.eulerAngles = currentRotation;
     }
 }
